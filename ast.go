bb1cb5506e2f39da462c62d1e27faafa376a561f
kamakuni <superradio86@gmail.com>
2018-05-13 06:28:14 +0900

    add expressions enum

--- ast.go
+++ ast.go
@@ -8,13 +8,38 @@ type Expr struct {
 type Expr_ int
 
 const (
+	// Block of statements
 	Block Expr_ = iota
+	// Add two expressions.
+	Add
+	// Subtract two expressions
+	Sub
+	// Multiply two expressions
+	Mul
+	// Divide two expressions
+	Div
+	// Variable expression
+	Variable
+	// Constant expression
+	Constant
+	// Assignment expression
+	Assign
+	// If expression 'if expr { expr } else { expr }'
+	If
+	// Function Call, first field is name of the function, second is list of arguments
+	Call
+	// Literal expression
+	Literal
+	// End of File
+	EOF
+	// Null
+	Nil
 )
 
-type Constant int
+type ConstantType int
 
 const (
-	String Constant = iota
+	String ConstantType = iota
 	Number
 	Bool
 )
