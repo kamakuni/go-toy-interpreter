548b643febd462e8711002c652f0544c7dbeb645
kamakuni <superradio86@gmail.com>
2018-05-24 23:49:34 +0900

    create some functions in parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"github.com/kamakuni/go-toy-interpreter/ast"
 	"github.com/kamakuni/go-toy-interpreter/lexer"
 )
 
@@ -38,7 +39,7 @@ func (p *Parser) tokenToString(tokenType lexer.TokenType) string {
 	return fmt.Sprint(tokenType)
 }
 
-func (p *Parser) unexpetedToken(ut string) {
+func (p *Parser) unexpectedToken(ut string) {
 	panic(fmt.Sprintf("Unexpected token found. Expected: %v, Found: %v instead.", ut, p.TokenStream.Tokens[p.CurrentIndex+1].TokenType))
 }
 
@@ -80,3 +81,162 @@ func (p *Parser) getCurrentNumber() float64 {
 		panic("Error while parsing to integer.")
 	}
 }
+
+/*
+func (p *Parser) Parse() ast.Expr {
+	var block []ast.Expr
+
+	// Read all tokens and create statements, then push it to the block.
+	for p.CurrentIndex < p.TokenCount {
+
+		// Ignore the current token if it is useless.
+		if p.Token.TokenType == lexer.Comment {
+			p.advanceToken()
+			continue
+		}
+
+		// Determine the parse type for current or (if not enough) next token.
+		var stmt ast.Expr
+		switch p.Token.TokenType {
+		case lexer.Keyword:
+			if p.Token.TokenType == "number" {
+				stmt = Expr{
+					node: p.ParseInteger(),
+				}
+			}
+			TokenType::Keyword(ref x) if x == "string" => {
+				Box::new(Expr {
+					span: None,
+					node: self.parse_string(),
+				})
+			}
+			TokenType::Keyword(ref x) if x == "bool" => {
+				Box::new(Expr {
+					span: None,
+					node: self.parse_bool(),
+				})
+			}
+			TokenType::Identifier(ref x) if x == "if" => {
+				Box::new(Expr {
+					span: None,
+					node: self.parse_if(),
+				})
+			}
+			TokenType::Identifier(ref x) => {
+				// Eat LParen
+				if self.eat_token("LParen") {
+					Box::new(Expr {
+						span: None,
+						node: self.parse_call(x.clone()),
+					})
+				} else {
+					self.unexpected_token("LParen");
+					unimplemented!();
+				}
+			}
+			TokenType::RBrace => break,
+			TokenType::EOF => {
+				block.push(Box::new(Expr {
+					span: None,
+					node: Expr_::EOF,
+				}));
+				break;
+			}
+			_ => {
+				self.unexpected_token(&self.token_to_string(&self.token.token_type));
+				Box::new(Expr {
+					span: None,
+					node: Expr_::Nil,
+				})
+			}
+		};
+
+	}
+}
+*/
+/*
+func (p *Parser) parseInteger() ast.Expr_ {
+	var identifier = ""
+	// Eat Identifier
+	if p.eatToken("Identifier") {
+		if p.Token.TokenType == lexer.Identifier {
+			identifier = p.Token.Value.(string)
+		} else {
+			// TODO
+		}
+		// Eat equal symbol (=)
+		if p.eatToken("Equals") {
+			//	return p.calculate(identifier)
+		} else {
+			p.unexpectedToken("Equals")
+		}
+	} else {
+		p.unexpectedToken("Identifier")
+	}
+	return ast.NIL
+}*/
+
+/**
+* Calculate arithmetic expression with Shunting-Yard Algorithm
+*/
+func (p *Parser) calculate(identifier String) Expr_ {
+	var operatorStack []lexer.TokenType
+	var rpn []lexer.RPNValue
+	var opPrecedences = make(map[ast.TokenType]int)
+	var waitExp = true;
+
+	// Push operators to precendeces list
+	opPrecedences[lexer.Plus] = 2
+	opPrecedences[lexer.Minus] = 2
+	opPrecedences[lexer.Multiple] = 3
+	opPrecedences[lexer.Divide] = 3
+	opPrecedences[lexer.Mod] = 3
+
+	// Loop for all numbers and operators
+	for {
+		/*if p.EatToken("Number") {
+			// Get first number
+			rpn = append(RPNValue::Number(self.get_current_number()));
+			wait_exp = false;
+		} else if wait_exp {
+			// If number is not set break the loop
+			break;
+		} else if self.eat_operator() {
+			// If eat an operator
+			let mut stack_len = operator_stack.len();
+
+			while stack_len > 0 &&
+					op_precedences.get(&self.token.token_type) <
+					op_precedences.get(&operator_stack[stack_len - 1]) {
+				rpn.push(RPNValue::Operator(operator_stack[stack_len - 1].to_owned()));
+				operator_stack.remove(stack_len - 1);
+				stack_len -= 1;
+			}
+
+			operator_stack.push(self.token.token_type.clone());
+			wait_exp = true;
+		} else {
+			// This means expression is ended and we need a semicolon check.
+			p.expect_semicolon();
+			break;
+		}*/
+	}
+
+	// wait_exp == true means line ended with an operator or line is empty.
+	if waitExp {
+		p.unexpectedToken("Number");
+	}
+
+	// Popping stack and pushing to rpn queue.
+	for op in operator_stack.iter().rev() {
+		rpn.push(RPNValue::Operator(op.to_owned()));
+	}
+
+	// Calling soveRPN function and returning it as Expr_.
+	Expr_::Assign(identifier,
+					Box::new(Expr {
+						span: None,
+						node: Expr_::Constant(Constant::Number(self.solve_rpn(rpn))),
+					}))
+}
+
