60cde5a42d19fbec19d1238b3abeba2d2eafbf7d
kamakuni <superradio86@gmail.com>
2018-06-12 07:38:54 +0900

    add path variable from args

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 )
 
 func main() {
+	var path = "src/test/main.c"
 	fmt.Println(os.Args)
+	if len(os.Args) > 1 {
+		fmt.Println("Your source file is: ", os.Args[1])
+		path = os.Args[1]
+	} else {
+		fmt.Println("You are using default test source path: src/test/main.c")
+	}
 	ts := lexer.CreateTokenStream("this is codes")
 	fmt.Println(ts)
 }
